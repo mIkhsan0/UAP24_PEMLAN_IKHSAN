9d75e2f12ac3d1b3919b3ecee85dfaeed79911ab
Muhammad Ikhsan <m3hs4n184@student.ub.ac.id>
2024-06-01 06:59:20 -0700

    .. selese

--- src/AksiAdmin.java
+++ src/AksiAdmin.java
@@ -23,9 +23,31 @@ public class AksiAdmin extends Aksi {
     @Override
     public void lihatListFilm() {
         // Implementasi melihat list film
+        Film.getFilms().forEach((key, value) -> {
+            System.out.print("Film: " + key);
+            System.out.print(" - Deskripsi: " + value.getDescription());
+            System.out.print(" - Harga: " + value.getPrice());
+            System.out.println(" - Stok: " + value.getStock());
+        });
     }
 
     public void tambahFilm() {
         // Implementasi menambahkan film
+        System.out.print("Nama Film: ");
+        Main.scanner.nextLine();
+        String filmName = Main.scanner.nextLine();
+        System.out.print("Deskripsi Film: ");
+        String description = Main.scanner.nextLine();
+        System.out.print("Harga Tiket: ");
+        double price = Main.scanner.nextDouble();
+        System.out.print("Stok Tiket: ");
+        int stock = Main.scanner.nextInt();
+        Film.addFilm(filmName, description, price, stock);
+
+        /*Film.addFilm("Furiosa: A Mad Max Saga", "The origin story of renegade warrior Furiosa before her encounter and teamup with Mad Max.", 100000, 10);
+        Film.addFilm("Kingdom of the Planet of the Apes", "Many years after the reign of Caesar, a young ape goes on a journey that will lead him to question everything he's been taught about the past and make choices that will define a future for apes and humans alike.", 150000, 5);
+        Film.addFilm("The Fall Guy", "A down-and-out stuntman must find the missing star of his ex-girlfriend's blockbuster film.", 200000, 7);
+        Film.addFilm("IF", "A young girl who goes through a difficult experience begins to see everyone's imaginary friends who have been left behind as their real-life friends have grown up.", 250000, 3);
+        Film.addFilm("Challengers", "Tashi, a former tennis prodigy turned coach, turned her husband into a champion. But to overcome a losing streak, he needs to face his ex-best friend and Tashi's ex-boyfriend.", 200000, 7);*/
     }
 }
--- src/AksiUser.java
+++ src/AksiUser.java
@@ -3,10 +3,11 @@ public class AksiUser extends Aksi {
     public void tampilanAksi() {
         System.out.println("Aksi User:");
         System.out.println("1. Pesan Film");
-        System.out.println("2. Lihat List Film");
-        System.out.println("3. Lihat Pesanan");
-        System.out.println("4. Logout");
-        System.out.println("5. Tutup Aplikasi");
+        System.out.println("2. Lihat Saldo");
+        System.out.println("3. Lihat List Film");
+        System.out.println("4. Lihat Pesanan");
+        System.out.println("5. Logout");
+        System.out.println("6. Tutup Aplikasi");
     }
 
     @Override
@@ -24,17 +25,54 @@ public class AksiUser extends Aksi {
     @Override
     public void lihatListFilm() {
         // Implementasi melihat list film
+        Film.getFilms().forEach((key, value) -> {
+            System.out.print("Film: " + key);
+            System.out.print(" - Deskripsi: " + value.getDescription());
+            System.out.print(" - Harga: " + value.getPrice());
+            System.out.println(" - Stok: " + value.getStock());
+        });
     }
 
     public void lihatSaldo() {
         // Implementasi lihat Saldo
+        System.out.println("Saldo Anda: " + (int) Akun.getCurrentUser().getSaldo());
     }
 
     public void pesanFilm() {
         // Implementasi pemesanan film
+        System.out.print("Nama Film yang ingin dipesan: ");
+        Main.scanner.nextLine();
+        String filmName = Main.scanner.nextLine();
+        if (!Film.getFilms().containsKey(filmName)) {
+            System.out.println("Film yang dicari tidak ditemukan.");
+            return;
+        }
+        System.out.print("Jumlah tiket yang ingin dipesan: ");
+        int jumlahTiket = Main.scanner.nextInt();
+        int stock = Film.getFilms().get(filmName).getStock();
+        if (jumlahTiket > stock) {
+            System.out.println("Stok tiket tidak mencukupi.");
+            return;
+        }
+        System.out.println("Harga satuan tiket " + Film.getFilms().get(filmName).getPrice());
+        System.out.println("Total harga: " + Film.getFilms().get(filmName).getPrice() * jumlahTiket);
+        if (Akun.getCurrentUser().getSaldo() < Film.getFilms().get(filmName).getPrice()) {
+            System.out.println("Saldo tidak mencukupi, saldo yang dimiliki " + (int) Akun.getCurrentUser().getSaldo());
+            return;
+        } else {
+            Film.getFilms().get(filmName).setStock(stock - jumlahTiket);
+            Akun.getCurrentUser().setSaldo(Akun.getCurrentUser().getSaldo() - Film.getFilms().get(filmName).getPrice());
+        }
+        Akun.getCurrentUser().addPesanan(Film.getFilms().get(filmName), jumlahTiket);
+        System.out.println("Tiket berhasil dipesan.");
     }
 
     public void lihatPesanan() {
         // Implementasi melihat pesanan
+        Akun.getCurrentUser().getPesanan().forEach((key, value) -> {
+            System.out.print("Film: " + key);
+            System.out.print(" - Jumlah Tiket: " + value.getKuantitas());
+            System.out.println(" - Total Harga: " + (int) value.getFilm().getPrice() * value.getKuantitas());
+        });
     }
 }
